2636f7f5e008ce784fda78dd2c2484642688cceb
urisawsing <72299686+urisawsing@users.noreply.github.com>
2021-12-27 16:19:38 +0100

    Update distributions.py

--- distributions.py
+++ distributions.py
@@ -3,3 +3,15 @@ import numpy as np
 
 def Gaussian(mu,sig,x):
   return (1/(sig*np.sqrt(2*np.pi)))*np.exp(-((x-mu)**2)/(2*sig**2))
+
+
+
+def generate(ndist,element):
+  individual=[]
+  if ndist == 1:
+    individual = gauss_gen(element)
+  else:
+    print("non correct individual")
+    
+  
+    
