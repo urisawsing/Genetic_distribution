72493d3741bb188c934315e971501f7c57e6a678
Maria Serracanta <93193590+mariaserracanta@users.noreply.github.com>
2022-01-03 16:25:22 +0100

    Update GA_main.py

--- adaptation/GA_main.py
+++ adaptation/GA_main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random as rnd
+import mystuff as ms
 
 #######################
 #  Genetic Algorithm  #
@@ -19,31 +20,31 @@ N=50
 sel=10
 
 #1. Setting the original population
-population=population_generator(N)
+population=ms.population_generator(N)
 for i in population:
-  population.sort(reverse=True, key=fitness_sort)
+  population.sort(reverse=True, key=ms.fitness_sort)
   
 #2. Selecting best individuals seted by now as progeny
 selected=population[:sel]
 
 #3. Applying mutation to the gamets of the selected progeny
-gamets=mutation(selected)
+gamets=ms.mutation(selected)
 
 #4. Defining the breeding pattern
 prog_1=gamets[0:sel/2]
 prog_2=gamets[sel:(sel/2)+1]
 
 #5. Crossover of the gamets generating two siblings
-offspring_1=crossover(prog_1, prog_2)
-offspring_2=crossover(prog_1, prog_2)
+offspring_1=ms.crossover(prog_1, prog_2)
+offspring_2=ms.crossover(prog_1, prog_2)
 
 #6. Adding variation by migration (new random generated population)
-migrants=population_generator(N-2*sel)
+migrants=ms.population_generator(N-2*sel)
 
 #7. Setting the new population
 new_population=offspring_1+offspring_2+migrants
 for i in new_population:
-  new_population.sort(reverse=True, key=fitness_sort)
+  new_population.sort(reverse=True, key=ms.fitness_sort)
 
 for i in new_population:
   print(i)
