68f6f480cc84e948bac269995dcc206f6adfa6a9
Maria Serracanta <93193590+mariaserracanta@users.noreply.github.com>
2022-01-03 16:52:31 +0100

    Update ms_main.py

--- adaptation/ms_main.py
+++ adaptation/ms_main.py
@@ -2,9 +2,10 @@ import numpy as np
 import random as rnd
 import mystuff as ms
 
-#######################
-#  Genetic Algorithm  #
-#######################
+##################################
+#  Genetic Algorithm in 7 steps  #
+##################################
+
 '''
 Parameters
     ----------
@@ -22,29 +23,28 @@ N=50
 elite=10
 bros=2
 
-
 #1. Setting the original population
 population=ms.population_generator(N)
 for i in population:
   population.sort(reverse=True, key=ms.fitness_sort)
   
-#2. Selecting best individuals seted by now as progeny
+#2. Selecting best individuals, from now on, the progeny
 selected=population[:sel]
 
-#3. Applying mutation to the gamets of the selected progeny
+#3. Generating the mutated gamets from the progeny
 gamets=ms.mutation(selected)
 
 #4. Defining the breeding pattern
 prog_1=gamets[0:sel/2]
 prog_2=gamets[sel:(sel/2)+1]
 
-#5. Crossover of the gamets generating the specifed siblings
+#5. Crossover of the gamets as times as the number of siblings specified
 offspring=[]
 for i in range(bros):
   birth=crossover(prog_1, prog_2)
   offspring.append(birth)
 
-#6. Adding variation by migration (new random generated population)
+#6. Adding variation by migration from a new random generated population
 migrants=ms.population_generator(N-2*sel)
 
 #7. Setting the new population
